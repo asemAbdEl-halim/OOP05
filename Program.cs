4913aa8cbc3e8f9a4b2ddaaf37e4f58204696d79
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-26 19:20:00 +0300

    Question02_Part01

--- Program.cs
+++ Program.cs
@@ -26,6 +26,34 @@
 
             #endregion
 
+            #region Question02
+
+            /*
+             * 2) Shallow Copy vs Deep Copy
+             * 
+             * a) What is a Shallow Copy?
+             * => Anser <==
+             * ==> A Shallow Copy creates a new object, but the reference-type members still point to the same objects.
+             * 
+             * b) What is a Deep Copy?
+             * => Anser <==
+             * ==> A Deep Copy creates a new object and also creates new copies of the reference-type members.
+             * 
+             * c) What happens to reference-type members in Shallow Copy?
+             * => Anser <==
+             * They point to the same object as the original.
+             * 
+             * d) What happens to reference-type members in Deep Copy?
+             * => Anser <==
+             * ==> They point to new separate objects, so changing one does not affect the other.
+             * 
+             * e) When is Deep Copy safer?
+             * => Anser <==
+             * ==> When you need to modify the copied object without changing the original object.
+             * **/
+
+            #endregion
+
             #endregion
         }
     }
