97543a57e727171c63513d7672a57a9f5bbf91c4
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-26 19:11:27 +0300

    Question01_Part01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            #region Question01
+
+            /*
+             * 1) - Object Copying
+             * a) What happens when you assign one object variable to another object variable?
+             * ==> Anser <==
+             * ==> When you assign one object variable to another, both variables refer to the same object in memory.
+             * 
+             * b) Does assigning one object to another create a new object? Explain.
+             * => Anser <==
+             * ==> No, assigning one object to another does not create a new object. It only copies the reference to the existing object.
+             * 
+             * c) What is the difference between copying an object and copying its reference?
+             * => Anser <==
+             * ==> Copying the object: creates a new separate object, so changing one does not affect the other.
+             * Copying the reference: both variables point to the same object, so changing it through one variable affects the other.
+             * **/
+
+            #endregion
+
+            #endregion
         }
     }
 }
